54b7a46cba7604e823bca3f16f2dfd73f38c39be
whis <wqcsimple@foxmail.com>
2015-02-03 17:11:32 +0800

    修改数据库连接信息

--- application/doc/configs/main.php
+++ application/doc/configs/main.php
@@ -4,9 +4,12 @@ return array(
 		 * 数据库参数
 */
 		'db'=>array(
-				'host'=>'localhost',					//数据库服务器
-				'user'=>'root',							//用户名
-				'password'=>'',							//密码
+// 				'host'=>'localhost',					//数据库服务器
+// 				'user'=>'root',							//用户名
+// 				'password'=>'',							//密码
+                'host'=>'114.215.134.73',
+		        'user'=>'whis',
+		        'password'=>'chen19921012',
 				'dbname'=>'whis_doc',						//数据库名
 				'charset'=>'utf8',						//数据库编码方式
 				'table_prefix'=>'whis_',					//数据库表前缀
